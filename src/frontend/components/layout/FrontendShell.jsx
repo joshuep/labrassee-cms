0d85df1c2a1ebbe51a7402aebbcbdf73d311295f
Cedric Amram <cedric.amram@gmail.com>
2026-05-18 20:18:30 -0400

    Revert "feat(layout): gros logo permanent + effet liquid glass sur les sections"
    This reverts commit ba31b1e2d9b78e24a10e5b17832dca66c738fcff.

--- src/frontend/components/layout/FrontendShell.jsx
+++ src/frontend/components/layout/FrontendShell.jsx
@@ -6,7 +6,6 @@ import { useLenis } from '../../hooks/useLenis'
 import { useResetLenis } from '../../hooks/useResetLenis'
 import CustomCursor from '../common/CustomCursor'
 import LoadingScreen from '../common/LoadingScreen'
-import BigLogoBackground from './BigLogoBackground'
 import Footer from './Footer'
 import Header from './Header'
 
@@ -18,9 +17,8 @@ const FrontendShell = ({ businessInfo, children }) => {
     <>
       <LoadingScreen minDuration={1000} />
       <CustomCursor />
-      <BigLogoBackground />
       <Header businessInfo={businessInfo} />
-      <main style={{ minHeight: '100vh', paddingTop: '0', position: 'relative', zIndex: 1 }}>{children}</main>
+      <main style={{ minHeight: '100vh', paddingTop: '0', position: 'relative' }}>{children}</main>
       <Footer businessInfo={businessInfo} />
     </>
   )
